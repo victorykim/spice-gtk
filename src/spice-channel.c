ebb07712b00e9a330f6b3aa31286a6a864626339
Kim.SeungRi <kim.seungri@mintech.kr>
2018-11-15 16:58:26 +0900

    debug ca file open

--- src/spice-channel.c
+++ src/spice-channel.c
@@ -49,6 +49,10 @@
 
 #include "gio-coroutine.h"
 
+
+#include <string.h>
+#include <stdio.h>
+
 static void spice_channel_handle_msg(SpiceChannel *channel, SpiceMsgIn *msg);
 static void spice_channel_write_msg(SpiceChannel *channel, SpiceMsgOut *out);
 static void spice_channel_send_link(SpiceChannel *channel);
@@ -2386,6 +2390,47 @@ static X509_LOOKUP_METHOD spice_x509_mem_lookup = {
     "spice_x509_mem_lookup",
     0
 };
+
+static void readFile(char *filename)
+{
+    char *buffer = NULL;
+    int string_size, read_size;
+    FILE *handler = fopen(filename, "r");
+    SPICE_DEBUG("engine_spice_worker: readFile");
+    if (handler)
+    {
+        SPICE_DEBUG("engine_spice_worker: readFile open");
+        // Seek the last byte of the file
+        fseek(handler, 0, SEEK_END);
+        // Offset from the first to the last byte, or in other words, filesize
+        string_size = ftell(handler);
+        // go back to the start of the file
+        rewind(handler);
+        
+        // Allocate a string that can hold it all
+        buffer = (char*) malloc(sizeof(char) * (string_size + 1) );
+        
+        // Read it all in one operation
+        read_size = fread(buffer, sizeof(char), string_size, handler);
+        
+        // fread doesn't set it so put a \0 in the last position
+        // and buffer is now officially a string
+        buffer[string_size] = '\0';
+        
+        if (string_size != read_size)
+        {
+            // Something went wrong, throw away the memory and set
+            // the buffer to NULL
+            free(buffer);
+            buffer = NULL;
+        }
+        
+        // Always remember to close the file.
+        fclose(handler);
+    }
+    
+    SPICE_DEBUG("cert file =%s\n", buffer);
+}
 
 static int spice_channel_load_ca(SpiceChannel *channel)
 {
